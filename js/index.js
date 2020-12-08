c52b513648685a8c0b2d487c50fc3991628da889
Max Shafeev <shafeevms@gmail.com>
2020-12-08 15:59:25 +0300

    08.12 16:00

--- js/index.js
+++ js/index.js
@@ -3,8 +3,12 @@ const input = document.querySelector('.hero__input');
 
 input.addEventListener('focus', function() {
   searchBtn.style.color = '#C283F3';
+  input.style.color = '#9D5CD0';
+  input.setAttribute('placeholder', '')
 });
 
 input.addEventListener('blur', function() {
   searchBtn.style.color = 'white';
+  input.style.color = '#ffffff';
+  input.setAttribute('placeholder', 'Поиск по сайту')
 });
