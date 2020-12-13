0abf36fb36a971dbe72bd4ebc899dc28719c59c1
Max Shafeev <shafeevms@gmail.com>
2020-12-13 22:29:55 +0300

    dropdown style

--- js/index.js
+++ js/index.js
@@ -26,62 +26,22 @@ var mySwiper = new Swiper('.hero', {
 })
 
 // dropdown
-
-const dropStore = {
-  real:
-  {
-    name: ['Тинторетто', 'Фридрих', 'Леонардо', 'Верроккьо'],
-    img: ['d1.jpg', 'd2.jpg', 'd3.jpg', 'd4.jpg']
-  },
-  impres:
-  {
-    name: ['Моне', 'Сислей', 'Мане', 'Ренуар'],
-    img: ['d5.jpg', 'd6.jpg', 'd7.jpg', 'd8.jpg'],
-  },
-  postimpres:
-  {
-    name: ['Ван Гог', 'Сезанн', 'Гоген', 'Сёра'],
-    img: ['d9.jpg', 'd10.jpg', 'd11.jpg', 'd12.jpg']
-  },
-  avangard:
-  {
-    name: ['Кандинский', 'Маринетти', 'Малевич', 'Пикассо'],
-    img: ['d13.jpg', 'd14.jpg', 'd15.jpg', 'd16.jpg']
-  },
-  futurizm:
-  {
-    name: ['Карра', 'Прателла', 'Северини', 'Балла'],
-    img: ['d17.jpg', 'd18.jpg', 'd19.jpg', 'd20.jpg']
-  },
-}
-
-const template = items => `
-  <div class="dropdown">
-    <ul class="dropdown__list reset" data-simplebar>
-      ${items}
-    </ul>
-  </div>`
-
-const dropTemplate = (title) => {
-  const names = dropStore[title].name;
-  let items = names.reduce((acc, el, index) => {
-    return acc += `<li class="dropdown__item d${index + 1}">
-                    <a href="#" class="dropdown__link">${el}</a>
-                  </li>`;
-  }, '')
-  return template(items);
-}
-
 document.addEventListener('click', function(e) {
-  document.querySelectorAll('.dropdown__await').forEach(el => el.innerHTML ='');
-
+  document.querySelectorAll('.dropdown').forEach(el => el.classList.remove('dropdown-visible'));
+  document.querySelectorAll('.hero__arrow').forEach(el => el.classList.remove('rotate'));
+  document.querySelectorAll('.hero__option').forEach(el => el.classList.remove('violet'));
   if (e.target.classList.contains('hero__option')) {
-    const parent = e.target.parentNode;
-    parent.querySelector('.dropdown__await').innerHTML = dropTemplate(parent.dataset.name)
-  } else {
-    document.querySelectorAll('.dropdown__await').forEach(el => el.innerHTML ='');
+    e.target.closest('.hero__select').querySelector('.dropdown').classList.add('dropdown-visible');
+    e.target.closest('.hero__item').querySelector('.hero__arrow').classList.add('rotate');
+    e.target.closest('.hero__select').querySelector('.hero__option').classList.add('violet');
   }
-});
+  else {
+    document.querySelectorAll('.dropdown').forEach(el => el.classList.remove('dropdown-visible'));
+    document.querySelectorAll('.hero__arrow').forEach(el => el.classList.remove('rotate'));
+    document.querySelectorAll('.hero__option').forEach(el => el.classList.remove('violet'));
+  }
+})
+
 
 // simplebar
 
